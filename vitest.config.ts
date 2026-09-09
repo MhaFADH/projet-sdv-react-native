662d45e1d7e5387b7ed6ea410565ffb15b71266d
Mohamed-Amine FADHLAOUI <fadhlaoui.mohameda@gmail.com>
2026-09-09 16:06:03 +0200

    chore: add coverage tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['__tests__/**/*.test.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'cobertura'],
+      include: ['{app,components,features,hooks,services,domain,theme,constants}/**/*.{ts,tsx}'],
+    },
   },
 });
